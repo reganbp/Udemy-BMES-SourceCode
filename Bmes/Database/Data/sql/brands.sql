2b394031d0c194c3d9a6f67106adb1bdca41d6a9
Chinedu Ogbu <techsafety2010@yahoo.com>
2020-01-31 15:30:06 +0000

    Commit number 7: Database Table Data

---
+++ Bmes/Database/Data/sql/brands.sql
@@ -0,0 +1,29 @@
+--
+-- File generated with SQLiteStudio v3.2.1 on Fri Jan 31 15:27:45 2020
+--
+-- Text encoding used: System
+--
+PRAGMA foreign_keys = off;
+BEGIN TRANSACTION;
+
+-- Table: Brands
+CREATE TABLE "Brands" (
+    "Id" INTEGER NOT NULL CONSTRAINT "PK_Brands" PRIMARY KEY AUTOINCREMENT,
+    "CreateDate" TEXT NOT NULL,
+    "ModifiedDate" TEXT NOT NULL,
+    "IsDeleted" INTEGER NOT NULL,
+    "Name" TEXT NULL,
+    "Slug" TEXT NULL,
+    "Description" TEXT NULL,
+    "MetaDescription" TEXT NULL,
+    "MetaKeywords" TEXT NULL,
+    "BrandStatus" INTEGER NOT NULL
+);
+INSERT INTO Brands (Id, CreateDate, ModifiedDate, IsDeleted, Name, Slug, Description, MetaDescription, MetaKeywords, BrandStatus) VALUES (1, '2019-09-05 12:11:26.9625642+01:00', '2019-09-05 12:11:26.9667414+01:00', 0, 'AMICO', 'AMICO', 'AMICO', 'AMICO', '0', 'amico');
+INSERT INTO Brands (Id, CreateDate, ModifiedDate, IsDeleted, Name, Slug, Description, MetaDescription, MetaKeywords, BrandStatus) VALUES (2, '2019-09-05 12:11:35.0107962+01:00', '2019-09-05 12:11:35.0108102+01:00', 0, 'ARDEX', 'ARDEX', 'ARDEX', 'ARDEX', '0', 'ardex');
+INSERT INTO Brands (Id, CreateDate, ModifiedDate, IsDeleted, Name, Slug, Description, MetaDescription, MetaKeywords, BrandStatus) VALUES (3, '2019-09-05 12:11:38.1845219+01:00', '2019-09-05 12:11:38.1845351+01:00', 0, 'CertainTeed', 'CertainTeed', 'CertainTeed', 'CertainTeed', '0', 'certainteed');
+INSERT INTO Brands (Id, CreateDate, ModifiedDate, IsDeleted, Name, Slug, Description, MetaDescription, MetaKeywords, BrandStatus) VALUES (4, '2019-09-05 12:11:39.7762466+01:00', '2019-09-05 12:11:39.7762591+01:00', 0, 'Sika Scofield', 'Sika Scofield', 'Sika Scofield', 'Sika Scofield', '0', 'sika-scofield');
+INSERT INTO Brands (Id, CreateDate, ModifiedDate, IsDeleted, Name, Slug, Description, MetaDescription, MetaKeywords, BrandStatus) VALUES (5, '2019-09-05 12:11:41.4107006+01:00', '2019-09-05 12:11:41.4107135+01:00', 0, 'SpecChem', 'SpecChem', 'SpecChem', 'SpecChem', '0', 'specchem');
+
+COMMIT TRANSACTION;
+PRAGMA foreign_keys = on;
---
+++ Bmes/Database/Data/sql/categories.sql
@@ -0,0 +1,32 @@
+--
+-- File generated with SQLiteStudio v3.2.1 on Fri Jan 31 15:28:05 2020
+--
+-- Text encoding used: System
+--
+PRAGMA foreign_keys = off;
+BEGIN TRANSACTION;
+
+-- Table: Categories
+CREATE TABLE "Categories" (
+    "Id" INTEGER NOT NULL CONSTRAINT "PK_Categories" PRIMARY KEY AUTOINCREMENT,
+    "CreateDate" TEXT NOT NULL,
+    "ModifiedDate" TEXT NOT NULL,
+    "IsDeleted" INTEGER NOT NULL,
+    "Name" TEXT NULL,
+    "Slug" TEXT NULL,
+    "Description" TEXT NULL,
+    "MetaDescription" TEXT NULL,
+    "MetaKeywords" TEXT NULL,
+    "CategoryStatus" INTEGER NOT NULL
+);
+INSERT INTO Categories (Id, CreateDate, ModifiedDate, IsDeleted, Name, Slug, Description, MetaDescription, MetaKeywords, CategoryStatus) VALUES (1, '2019-09-05 12:19:31.2151155+01:00', '2019-09-05 12:19:31.2193723+01:00', 0, 'Basins', 'Basins', 'Basins', 'Basins', '0', 'basins');
+INSERT INTO Categories (Id, CreateDate, ModifiedDate, IsDeleted, Name, Slug, Description, MetaDescription, MetaKeywords, CategoryStatus) VALUES (2, '2019-09-05 12:19:35.3914241+01:00', '2019-09-05 12:19:35.3914351+01:00', 0, 'Baths', 'Baths', 'Baths', 'Baths', '0', 'baths');
+INSERT INTO Categories (Id, CreateDate, ModifiedDate, IsDeleted, Name, Slug, Description, MetaDescription, MetaKeywords, CategoryStatus) VALUES (3, '2019-09-05 12:19:36.4270588+01:00', '2019-09-05 12:19:36.4270806+01:00', 0, 'Ceiling Works', 'Ceiling Works', 'Ceiling Works', 'Ceiling Works', '0', 'ceiling-works');
+INSERT INTO Categories (Id, CreateDate, ModifiedDate, IsDeleted, Name, Slug, Description, MetaDescription, MetaKeywords, CategoryStatus) VALUES (4, '2019-09-05 12:19:37.3761237+01:00', '2019-09-05 12:19:37.3761351+01:00', 0, 'Doors and Windows', 'Doors and Windows', 'Doors and Windows', 'Doors and Windows', '0', 'doors-and-windows');
+INSERT INTO Categories (Id, CreateDate, ModifiedDate, IsDeleted, Name, Slug, Description, MetaDescription, MetaKeywords, CategoryStatus) VALUES (5, '2019-09-05 12:19:38.5843523+01:00', '2019-09-05 12:19:38.5843629+01:00', 0, 'Electrical Works', 'Electrical Works', 'Electrical Works', 'Electrical Works', '0', 'electrical-works');
+INSERT INTO Categories (Id, CreateDate, ModifiedDate, IsDeleted, Name, Slug, Description, MetaDescription, MetaKeywords, CategoryStatus) VALUES (6, '2019-09-05 12:19:39.598841+01:00', '2019-09-05 12:19:39.598852+01:00', 0, 'Painting Works', 'Painting Works', 'Painting Works', 'Painting Works', '0', 'painting-works');
+INSERT INTO Categories (Id, CreateDate, ModifiedDate, IsDeleted, Name, Slug, Description, MetaDescription, MetaKeywords, CategoryStatus) VALUES (7, '2019-09-05 12:19:41.0836632+01:00', '2019-09-05 12:19:41.0836742+01:00', 0, 'Pipes and Fittings', 'Pipes and Fittings', 'Pipes and Fittings', 'Pipes and Fittings', '0', 'pipes-and-fittings');
+INSERT INTO Categories (Id, CreateDate, ModifiedDate, IsDeleted, Name, Slug, Description, MetaDescription, MetaKeywords, CategoryStatus) VALUES (8, '2019-09-05 12:19:42.165861+01:00', '2019-09-05 12:19:42.1658817+01:00', 0, 'Tile Works', 'Tile Works', 'Tile Works', 'Tile Works', '0', 'tile-works');
+
+COMMIT TRANSACTION;
+PRAGMA foreign_keys = on;
